2e983ba97744c3d1c675e0de61b6ecd85344714f
Amity808 <bolarinwamuhdsodiq0@gmail.com>
2024-08-28 11:32:31 +0100

    fix bug

--- frontend/src/components/ui/zeta/toaster.tsx
+++ frontend/src/components/ui/zeta/toaster.tsx
@@ -5,8 +5,8 @@ import {
   ToastProvider,
   ToastTitle,
   ToastViewport,
-} from "@/components/ui/toast"
-import { useToast } from "@/components/ui/use-toast"
+} from "@/components/ui/zeta/toast"
+import { useToast } from "@/components/ui/zeta/use-toast"
 
 export function Toaster() {
   const { toasts } = useToast()
